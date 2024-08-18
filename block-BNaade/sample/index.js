17e22c893af608b889965f1c0ce7128bfc150fc5
suryawanshishreyas <suryawanshishreyas@gmail.com>
2024-08-18 13:50:56 +0530

    update feature

--- block-BNaade/sample/index.js
+++ block-BNaade/sample/index.js
@@ -1,17 +1,46 @@
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
+// require setup
+var express = require('express');
+var mongoose = require('mongoose');
+var path = require('path');
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
 
-// app.get('/',(req,res)=>{
-//     res.send("Hello")
-// })
+// connecting to db
+mongoose.connect('mongodb://127.0.0.1:27017/userDiary2',
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology: true
+    }
+).then(()=>{
+    console.log('Successfully connected to Database');
+}).catch(()=>{
+    console.log(`Failed to connect to Database`);
+})
+// Mounting express app
+var app = express();
 
-app.use(cookieParser())
-app.use(express.static('public'))
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
 
-app.set('view engine', 'ejs')
+// setup view engine
+app.set("view engine","ejs");
+app.set('views',path.join(__dirname,"views"));
 
+// routing middlewares
+app.use('/',indexRouter);
+app.use('/users',userRouter);
+
+// error handling middlewares
+app.use((req,res,next)=>{
+    res.send('Page Not Found');
+})
+
+app.use((err,req,res,next)=>{
+    res.send(err);
+})
 
 app.listen(3000,()=>{
-    console.log('App is listening on port 3000');
+    console.log(`Server is listening on port 3000`);
 })
